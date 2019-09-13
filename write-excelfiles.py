06c506aadc7a0c71b9a6566c5b0ace005ca6b2df
Karthikeyan Sivaraman <octotus@users.noreply.github.com>
2019-09-12 20:40:20 -0400

    Add files via upload

--- write-excelfiles.py
+++ write-excelfiles.py
@@ -11,10 +11,11 @@ for files in annot_file_list:
 	sheet = files.split('.')[0]
 	open_sheet = oFile.add_worksheet(sheet)
 	for x in F.readlines():
-		if(line_num == 1):
-			open_sheet.write_row(0,1,x)
-		else:
-			open_sheet.write_row(line_num,0,x)
+		array=x.split("\t")
+		for col_ind,y in enumerate(array):
+			if(line_num == 0):
+				open_sheet.write_string(0,col_ind+1,y)
+			else:
+				open_sheet.write_string(line_num,col_ind,y)
 		line_num = line_num+1
 oFile.close()
-
